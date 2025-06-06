6548974f7a1e168d6737ed50c4cbd208d42f97ff
GustavoCarlini <gcarlini52@gmail.com>
2025-06-06 12:24:58 -0300

    Agregado Mapa

--- main.js
+++ main.js
@@ -16,6 +16,26 @@ document.addEventListener("DOMContentLoaded", () => {
   includeHTML("header", "/components/header.html");
   includeHTML("footer", "/components/footer.html");
 
+  //MAPAS
+  const latBattios = -31.245279091786315;
+  const lonBattios = -61.48525198165976;
+
+  const map = L.map('map').setView([latBattios,lonBattios], 16);
+
+
+L.tileLayer (`https://maps.geoapify.com/v1/tile/osm-carto/{z}/{x}/{y}.png?apiKey=bcc9afc699fd480681415a6e281c1ff8`, {
+   attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors | © Geoapify', 
+   maxZoom: 20, 
+}).addTo(map);
+
+
+L.marker([latBattios,lonBattios])
+    .addTo(map)
+    .bindPopup("<b>Battios</b><br>Güemes 846")
+    .openPopup();
+
+
+
   // FUNCIONALIDAD DEL MENÚ HAMBURGUESA
   function setupMenuToggle() {
     const nav = document.querySelector(".navbar");
