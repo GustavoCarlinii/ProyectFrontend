c5d9ed04b9da32ca70cfaca0974243d42d7d9955
GustavoCarlini <gcarlini52@gmail.com>
2025-06-19 11:36:15 -0300

    Agregado el Formulario

--- main.js
+++ main.js
@@ -53,3 +53,59 @@ L.marker([latBattios,lonBattios])
     }
   }
 });
+
+
+//FORMULARIO 
+const form = document.getElementById("contactoForm");
+
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      let valido = true;
+
+      const nombre = document.getElementById("nombre");
+      const apellido = document.getElementById("apellido");
+      const email = document.getElementById("email");
+      const telefono = document.getElementById("telefono");
+      const motivo = document.getElementById("motivo");
+      const mensaje = document.getElementById("mensaje");
+
+     
+      document.querySelectorAll(".error").forEach(e => e.textContent = "");
+
+      // VALIDACIONES
+      if (nombre.value.trim() === "") {
+        document.getElementById("errorNombre").textContent = "El nombre es obligatorio.";
+        valido = false;
+      }
+
+      if (apellido.value.trim() === "") {
+        document.getElementById("errorApellido").textContent = "El apellido es obligatorio.";
+        valido = false;
+      }
+
+      if (email.value.trim() === "" || !/^\S+@\S+\.\S+$/.test(email.value)) {
+        document.getElementById("errorEmail").textContent = "Email inválido.";
+        valido = false;
+      }
+
+      if (telefono.value.trim() === "" || !/^\d{6,15}$/.test(telefono.value)) {
+        document.getElementById("errorTelefono").textContent = "Número inválido (solo dígitos, 6-15 caracteres).";
+        valido = false;
+      }
+
+      if (motivo.value === "") {
+        document.getElementById("errorMotivo").textContent = "Seleccioná un motivo.";
+        valido = false;
+      }
+
+      if (mensaje.value.trim().length < 10) {
+        document.getElementById("errorMensaje").textContent = "El mensaje debe tener al menos 10 caracteres.";
+        valido = false;
+      }
+
+      if (valido) {
+        alert("Formulario enviado correctamente ✅");
+        form.reset();
+      }
+    });
